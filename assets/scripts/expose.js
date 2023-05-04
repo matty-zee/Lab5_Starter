6a5f098ac3c482b698e043108de9f5b2ed39a9bd
matty-zee <97648068+matty-zee@users.noreply.github.com>
2023-05-04 12:24:46 -0700

    starting volume slider event handler

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,24 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  function handleVolChange(Event){
+
+    const volumeLevel = Event.target.value;
+    const volPic = document.getElementById('volume-controls').getElementsByTagName("img").src;
+
+    if(volumeLevel == 0){
+      volPic = "volume-level-0.svg";
+    }else if(volumeLevel < 33){
+      volPic = "volume-level-1.svg";
+    }else if(volumeLevel < 67){
+      volPic = "volume-level-2.sng";
+    }else{
+      volPic = "volume-level-3.svg";
+    }
+  }
+
 }
+
+const volSlider = document.getElementById('volume');
+volSlider.addEventListener('change', handleVolChange);
