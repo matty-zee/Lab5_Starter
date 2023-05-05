e44dd71e0a8e5e06258eaf85351f167f66219c46
matty-zee <97648068+matty-zee@users.noreply.github.com>
2023-05-05 11:52:41 -0700

    finishing speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,17 +3,61 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const voiceSelect = document.querySelector("select");
-  window.addEventListener('DOMContentLoaded', init);
+  const button = document.getElementsByTagName("button")[0];
+  const inputTxt = document.getElementById("text-to-speak");
+  const facePic = document.getElementsByTagName("img")[0];
+
   let voices = [];
-  window.speechSynthesis.onvoiceschanged= function(){
-    voices = window.speechSynthesis.getVoices();
+  let utterThis;
+  let synth = window.speechSynthesis;
+
+  setInterval(function() {
+    if(synth.speaking){
+      facePic.src = "assets/images/smiling-open.png";
+    }else{
+      facePic.src = "assets/images/smiling.png";
+    }
+  }, 100);
+
+
+
+  button.addEventListener("click", handleButtonClick)
+  voiceSelect.addEventListener("input", handleVoiceSelect);
+  inputTxt.addEventListener("input", handleTxtInput);
+
+  synth.onvoiceschanged= function(){
+    voices = synth.getVoices();
     populateVoiceList();
+  }
+
+
+
+  function handleVoiceSelect(Event){
+    const voiceType = Event.target.value; 
+    
+
+    for(let i=0; i < voices.length; i++){
+      if(voices[i].name == voiceType){
+        utterThis.voice = voices[i];
+      }
+    }
+
+  }
+
+  function handleTxtInput(Event){
+    const spokenText = Event.target.value;
+    console.log(spokenText);
+    utterThis = new SpeechSynthesisUtterance(spokenText);
+  }
+
+  function handleButtonClick(Event) {
+    synth.speak(utterThis);
   }
 
   function populateVoiceList() {
     for(let i=0; i < voices.length; i++){
       const option = document.createElement("option");
-      option.textContent = voices[i].name + voices[i].lang;
+      option.textContent = voices[i].name;
   
       if(voices[i].default){
       option.textContent += " - Default"
@@ -22,7 +66,6 @@ function init() {
       option.setAttribute("data-lang", voices[i].lang);
       option.setAttribute("data-name", voices[i].name);
       voiceSelect.appendChild(option);
-      console.log(i);
     }
     
   }
