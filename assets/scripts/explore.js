b483e81fde8cca5d571126e24456bfa48e01904d
matty-zee <mzheng1108@gmail.com>
2023-05-04 23:45:13 -0700

    starting part 2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,31 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.querySelector("select");
+  window.addEventListener('DOMContentLoaded', init);
+  let voices = [];
+  window.speechSynthesis.onvoiceschanged= function(){
+    voices = window.speechSynthesis.getVoices();
+    populateVoiceList();
+  }
+
+  function populateVoiceList() {
+    for(let i=0; i < voices.length; i++){
+      const option = document.createElement("option");
+      option.textContent = voices[i].name + voices[i].lang;
+  
+      if(voices[i].default){
+      option.textContent += " - Default"
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+      console.log(i);
+    }
+    
+  }
+
+  
 }
